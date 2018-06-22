08fc8856e847b7f943d79c2293b6aba754b95eb5
Isaac Weiss <Iweiss20@wooster.edu>
2018-06-22 11:09:08 -0400

    Finished step 1

--- Tricolorability.py
+++ Tricolorability.py
@@ -13,32 +13,43 @@ def main():
 
 def build_array(strands):
     """
-
-    :param strands:
-    :return:
+    Builds up the array needed to calculate number of colorings of a prime knot
+    :param strands: the number of strands a given knot has
+    :return: an array with the proper equations to solve the coloring question
     """
 
-    # gauss_code = list(input("Please input  extended gauss code: "))
+    # 1 2 6 6 1 2 5 6 1 3 4 2 4 5 3 5 4 3
+
     gauss_code = [int(x) for x in input("Please input strand code: ")
         .split()]
-    print(gauss_code)
-    array = [strands]
-    array.append([strands])
+
+    array = []
+
+    for i in range(0, strands):
+        array.append([0] * strands)
+
 
     for i in range(0, strands):
         if i == 0:
-            j = abs(gauss_code[i+1])
-            k = abs(gauss_code[i+2])
-            array[i][i] = 1
-            array[i][j - 1] = 1
-            array[i][k - 1] = 1
+            array[0][0] = 1
+            array[0][abs(gauss_code[1]) - 1] = 1
+            array[0][abs(gauss_code[2]) - 1] = 1
         else:
-            j = abs(gauss_code[3 * i + 1])
-            k = abs(gauss_code[3 * i + 2])
-            array[i][i] = 1
-            array[i][j - 1] = 1
-            array[i][k - 1] = 1
+            array[i][gauss_code[3 * i] - 1] = 1
+            array[i][abs(gauss_code[3 * i + 1]) - 1] = 1
+            array[i][abs(gauss_code[3 * i + 2]) - 1] = 1
 
+    print(array)
+
+    return array
+
+
+def nullspace(array):
+    """
+
+    :param array:
+    :return:
+    """
 
     return array
 
