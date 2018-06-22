df52fe3cce2a9d4849672359c8844eeec323f69b
Isaac Weiss <Iweiss20@wooster.edu>
2018-06-22 13:08:33 -0400

    begin matlab algorithm

--- Tricoloration.m
+++ Tricoloration.m
@@ -1,2 +1,17 @@
 close all;
 clear all;
+
+strands = int(input("How many strands do you have? "));
+
+%Users need to input as a list
+gauss_code = int(input("Please input strand code: "));
+
+array = [strand][strand];
+
+for i=1:strands
+    array[i][gauss_code[3 * i]] = 1;
+    array[i][gauss_code[3 * i + 1]] = 1;
+    array[i][gauss_code[3 * i + 2]] = 1;
+end;
+
+rref_array = rref(array);
