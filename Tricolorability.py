12afb328195a014b6aecb2042b2e6056bf0a760a
Isaac Weiss <Iweiss20@wooster.edu>
2018-06-22 10:16:47 -0400

    first commit

--- Tricolorability.py
+++ Tricolorability.py
@@ -1,6 +1,36 @@
 def main():
-  return 6
-  
-  
-if __name__=='__main__':
-  main()
+    """
+
+    :return: 0
+    """
+
+    arcs = int(input("How many arcs do you have? "))
+
+    array = build_array(arcs)
+
+    return 0
+
+
+def build_array(arcs):
+    """
+
+    :param arcs:
+    :return:
+    """
+
+    # gauss_code = list(input("Please input  extended gauss code: "))
+    gauss_code = [int(x) for x in input("Please input  extended gauss code: ")
+        .split()]
+    print(gauss_code)
+    array = []
+    array.append([])
+
+    if gauss_code[0] < 0:
+        gauss_code.append(gauss_code[0])
+
+    # for i in range(arcs):
+
+    return array
+
+if __name__== '__main__':
+    main()
