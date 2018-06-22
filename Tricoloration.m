47f6b3fc308d1ae2b53243651e8fc37d2d9cd1ce
Isaac Weiss <Iweiss20@wooster.edu>
2018-06-22 16:03:10 -0400

    More debugging

--- Tricoloration.m
+++ Tricoloration.m
@@ -1,11 +1,12 @@
 close all;
 clear all;
 
-strands = input('How many strands do you have? ');
+strands = 4; %= input('How many strands do you have? ');
 % int_strands = str2num(strands);
 %Users need to input as a list
 % gauss_code = input('Please input strand code: ');
-gauss_code = [1 2 6 6 2 1 5 6 1 3 4 2 4 5 3 5 4 3];
+% gauss_code = [1 2 6 6 2 1 5 6 1 3 4 2 4 5 3 5 4 3];
+gauss_code = [1 3 2 3 1 4 4 1 2 2 3 4];
 % int_gauss_code = str2num(gauss_code);
 array = zeros(strands, strands);
 
@@ -25,8 +26,26 @@ array = zeros(strands, strands);
      end
  end
  
- array(strands, 1:strands) = 0;
+%  array(strands, 1:strands) = 0;
+array = rref(array);
+ counter = 0;
+ for i=1:strands
+     check = true;
+     for j = 1:strands
+         if array(i,j)==0
+             check = true;
+         else
+             check = false;
+             break
+         end
+     end
+     
+     if check == true
+         counter = counter + 1;
+     end
+ end
+ 
+ Nullity = 3 ^ counter;
+ 
 
- Nullity = 3^(length(null(rref(array))));
-
-%  print(Nullity);
+ disp(Nullity);
